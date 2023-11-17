f602e4a918a1eb700066bb53b3ba9d448447d678
akira <yen2419@gmail.com>
2023-11-17 19:51:56 +0800

    Add edit user information handler
    fix some minor bug in previous commit

--- src/routes/api/v1/users/index.js
+++ src/routes/api/v1/users/index.js
@@ -1,14 +1,15 @@
 // in src/routes/api/v1/users/index.js
 import { Router } from 'express';
-import { getAllUsers, registerHandler, loginHandler } from './handler.js';
+import { getCurrentUser, registerHandler, loginHandler, updateHandler } from './handler.js';
 
 const router = Router();
 
-//TODO: add something here
-router.get('/', getAllUsers);
+router.get('/', getCurrentUser);
 
 router.post('/register', registerHandler);
 
 router.post('/login', loginHandler);
+
+router.put('/update', updateHandler);
 
 export default router;
